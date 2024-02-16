dd129935b22716bb0c6b769508671dce93a204d2
rakib0miah <rm0066.bd@gmail.com>
2024-02-16 21:01:26 +0600

    Final score , clear privious Alphabet and Quit Game

--- script/alphabet-clash.js
+++ script/alphabet-clash.js
@@ -9,6 +9,10 @@
 
 function handleKeyboardKeyUpEvent(event) {
   const pleyerPressed = event.key;
+  console.log(pleyerPressed)
+  if( pleyerPressed === "Escape"){
+    gameOver();
+  }
 
   // console.log(pleyerPressed)
 
@@ -19,33 +23,46 @@ function handleKeyboardKeyUpEvent(event) {
   // console.log(pleyerPressed,expectedAlphabet)
 
   // check matched or not
-  if (pleyerPressed === expectedAlphabet) {
-    // console.log('You got a point');
-    // update score:
-    // 1. get the current score
-    const currentScoreElement = document.getElementById("current-score");
-    const currentScoreText = currentScoreElement.innerText;
-    const currentScore = parseInt(currentScoreText);
-
-    // 2. increase the score by 1
-    const newScore = currentScore + 1;
-    console.log(newScore);
-
-    // 3. show the update score 
-    currentScoreElement.innerText = newScore
+  if (pleyerPressed === expectedAlphabet){
+    const currentScore = getTextElementValueById('current-score');
+    const updatedScore = currentScore + 1;
+    setTextElementValueBId('current-score', updatedScore)
+    // __________________________________________________________________
+    // // console.log('You got a point');
+    // // update score:
+    // // 1. get the current score
+    // const currentScoreElement = document.getElementById("current-score");
+    // const currentScoreText = currentScoreElement.innerText;
+    // const currentScore = parseInt(currentScoreText);
+    // // 2. increase the score by 1
+    // const newScore = currentScore + 1;
+    // // console.log(newScore);
+    // // 3. show the update score 
+    // currentScoreElement.innerText = newScore
     removeBackgroundColorById(expectedAlphabet);
     continueGame();
   } else {
-    console.log("You loss a point");
-    // step 1: get the current life number 
-    const currentLifeElement = document.getElementById('cureent-life');
-    const currentLifeText = currentLifeElement.innerText;
-    const currentLife = parseInt(currentLifeText);
+    const currentLife = getTextElementValueById('current-life');
+    const updatedLife = currentLife - 1;
+    setTextElementValueBId('current-life', updatedLife)
+    // ________________________________________________________________
+    // // console.log("You loss a point");
+    // // step 1: get the current life number 
+    // const currentLifeElement = document.getElementById('cureent-life');
+    // const currentLifeText = currentLifeElement.innerText;
+    // const currentLife = parseInt(currentLifeText);
 
-    // step 2: reduce current life 
-    const newLife = currentLife - 1;
-    // step 3: display the updated life count 
-    currentLifeElement.innerText = newLife
+    // // step 2: reduce current life 
+    // const newLife = currentLife - 1;
+    // // step 3: display the updated life count 
+    // currentLifeElement.innerText = newLife
+
+    if(updatedLife === 0){
+        gameOver();
+
+    }
+
+
   }
 }
 
@@ -65,7 +82,26 @@ function continueGame() {
 }
 
 function play() {
+    // hide everything show only the playground 
   hideElementById("home-screen");
+  hideElementById('final-score')
   showElementById("play-ground");
+
+//   reset score and life 
+setTextElementValueBId('current-life', 5);
+setTextElementValueBId('current-score', 0)
   continueGame();
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score')
+    // update final score 
+    // 1.get the final score 
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueBId('game-score', lastScore);
+
+    // clear the last selected alphabet highlight 
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet)
 }
